bc114e065800829fd2d9ed5d9f621ca7f8831272
Vanshika Bhardwaj <vanshika.bhardwaj.14@gmail.com>
2018-06-27 19:33:34 +0530

    project

---
+++ racecar.py
@@ -0,0 +1,71 @@
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (127,127,127)
+red = (255,0,0)
+
+car_width = 73
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load('racecar.png')
+
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+    y_change = 0
+    gameExit = False
+
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+                if event.key == pygame.K_UP:
+                    y_change = -5
+                if event.key == pygame.K_DOWN:
+                    y_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = 0
+
+        x += x_change
+        y += y_change
+
+        gameDisplay.fill(white)
+        car(x,y)
+
+        if x > display_width - car_width or x < 0:
+            gameExit = True
+            
+        
+        pygame.display.update()
+        clock.tick(60)
+
+
+
+game_loop()
+pygame.quit()
+quit()
